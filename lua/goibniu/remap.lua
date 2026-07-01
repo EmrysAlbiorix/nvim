6664804aebef0bc7e467409ec0187d963fd8bc65
Emrys <psychedover@gmail.com>
2026-06-30 23:44:18 -0700

    Added git branch tree keymap "<leader>gl"

--- lua/goibniu/remap.lua
+++ lua/goibniu/remap.lua
@@ -3,11 +3,27 @@ vim.g.mapleader = " "
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- === === === === === === === === === ===
+--              FILE CONTROLS
+-- === === === === === === === === === ===
+
+-- Main controls
 vim.keymap.set("n", "<leader>pv", ":Oil<CR>", { desc = "Open parent directory" })
 vim.keymap.set("n", "<leader>rl", ":update<CR> :source ~/.config/nvim/init.lua<CR>", { desc = "Reload lua config" })
 vim.keymap.set("n", "<leader>so", ":so<CR>", { desc = "Reload current file" })
 vim.keymap.set("n", "<leader>w", ":write<CR>", { desc = "Save current file" })
 vim.keymap.set("n", "<leader>q", ":quit<CR>", { desc = "Quit vim" })
+
+-- Reformats the current file
+vim.keymap.set("n", "<leader>rf", function()
+	require("conform").format()
+end, { desc = "Format current file" })
+
+-- === === === === === === === === === ===
+--            WINDOW CONTROLS
+-- === === === === === === === === === ===
+
+-- Create windows
 vim.keymap.set("n", "<leader>sph", ":sp<CR>", { desc = "Split horizontally" })
 vim.keymap.set("n", "<leader>sp", ":vsp<CR>", { desc = "Split vertically" })
 vim.keymap.set("n", "<leader>nt", ":tabe", { desc = "Open new tab [filename]" })
@@ -24,6 +40,10 @@ vim.keymap.set("n", "<leader>=", ":resize +2<CR>", { desc = "Increase window hei
 vim.keymap.set("n", "<leader><left>", ":vertical resize -2<CR>", { desc = "Decrease window width" })
 vim.keymap.set("n", "<leader><right>", ":vertical resize +2<CR>", { desc = "Increase window width" })
 
+-- === === === === === === === === === ===
+--           MAIN GIT COMMANDS
+-- === === === === === === === === === ===
+
 -- Git functionality (using vimscript)
 vim.keymap.set("n", "<leader>gs", ":Git<CR>", { desc = "Git status" })
 vim.keymap.set("n", "<leader>ga", ":Git add .<CR>", { desc = "Git add" })
@@ -31,17 +51,25 @@ vim.keymap.set("n", "<leader>gc", ":Git commit<CR>", { desc = "Git commit" })
 vim.keymap.set("n", "<leader>gp", ":Git push<CR>", { desc = "Git push" })
 vim.keymap.set("n", "<leader>gl", ":Git pull<CR>", { desc = "Git pull" })
 vim.keymap.set("n", "<leader>gd", ":Gdiff<CR>", { desc = "Git diff" })
-
--- Trying to get git blame to work...
 vim.keymap.set("n", "<leader>gb", ":Git blame<CR>", { desc = "Git blame" })
---vim.keymap.set("n", "<leader>gb", function()
---  return pcall(vim.cmd, "Gblame")
---end, { desc = "Git blame" })
 
--- Reformats the current file
-vim.keymap.set("n", "<leader>rf", function()
-	require("conform").format()
-end, { desc = "Format current file" })
+-- === === === === === === === === === ===
+--            GIT BRANCH TREE
+-- === === === === === === === === === ===
+
+-- Tree of all branches
+vim.keymap.set("n", "<leader>gt", function()
+	vim.cmd("Git log --graph --all --oneline --decorate")
+end, { desc = "Git log graph (all branches)" })
+
+-- Tree of current branch
+vim.keymap.set("n", "<leader>gT", function()
+	vim.cmd("Git log --graph --oneline --decorate")
+end, { desc = "Git log graph (current branch)" })
+
+-- === === === === === === === === === ===
+--           TERMINAL COMMANDS
+-- === === === === === === === === === ===
 
 -- Open terminal with <Space> ft
 vim.keymap.set("n", "<leader>t", function()
@@ -50,6 +78,10 @@ end, { desc = "Open terminal" })
 
 -- Exit terminal mode with Esc
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>:q<CR>", { desc = "Exit and Close terminal" })
+
+-- === === === === === === === === === ===
+--            MINUET COMMANDS
+-- === === === === === === === === === ===
 
 -- Minuet Keybinds
 vim.keymap.set("n", "<leader>mp", "<cmd>Minuet duet predict<cr>", { desc = "Minuet duet predict" })
