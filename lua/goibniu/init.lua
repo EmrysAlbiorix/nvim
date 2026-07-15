675342c799022c204ed5c54471c36f9723f51b8d
Emrys <psychedover@gmail.com>
2026-07-14 20:54:56 -0700

    Ensured that all keymaps have desc (I think)

--- lua/goibniu/init.lua
+++ lua/goibniu/init.lua
@@ -69,7 +69,7 @@ vim.lsp.enable({
 })
 
 -- Essential LSP keymaps
-vim.keymap.set("n", "K", vim.lsp.buf.hover)
+vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show hover documentation" })
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
 vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Show references" })
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
